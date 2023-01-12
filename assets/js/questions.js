7e1eb6caa16cd5f7ff4e7233f7c847afa4116885
JayaPK21 <80857811+JayaPK21@users.noreply.github.com>
2023-01-12 13:17:41 +0000

    Added the proper questions for the quiz

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -12,32 +12,35 @@ var button4El = document.createElement("button");
 var buttonList = [button1El, button2El, button3El, button4El];
 
 var quizObjOne = {
-    question : "first question?",
-    choiceList : ["option1", "option2", "option3", "option4"],
-    correctAnswer : 1
+    question : "Inside which HTML element do we put the JavaScript?",
+    choiceList : ["<js>", "<javascript>", "<script>", "<scripting>"],
+    correctAnswer : 2
 };
 
 var quizObjTwo = {
-    question : "second question?",
-    choiceList : ["a", "b", "optionc", "optiond"],
+    question : "How do you call a function named \"myFunction\"?",
+    choiceList : ["myFunction()", "call myFunction()", "call function myFunction()", "myFunction"],
     correctAnswer : 0
 };
 
 var quizObjThree = {
-    question : "third question?",
-    choiceList : ["optiona", "b", "optionc", "optiond"],
-    correctAnswer : 1
+    question : "How can you add a comment in JavaScript?",
+    choiceList : ["'This is a comment", "<!-- This is a comment -->", "// This is a comment", "(This is a comment)"],
+    correctAnswer : 2
 };
 
 var quizObjFour = {
-    question : "fourth question?",
-    choiceList : ["optiona", "optionb", "optionc", "optiond"],
-    correctAnswer : 0
+    question : "How do you create Arrays in JavaScript?",
+    choiceList : ["var colors = (1:\"red\", 2:\"green\", 3:\"blue\")", 
+                    "var colors = [\"red\", \"green\", \"blue\"]",
+                    "var colors = 1 = (\"red\"), 2 = (\"green\"), 3 = (\"blue\")",
+                    "var colors = \"red\", \"green\", \"blue\""],
+    correctAnswer : 1
 };
 
 var quizObjFive = {
-    question : "fifth question?",
-    choiceList : ["a", "b", "optionc", "optiond"],
+    question : "Which of the following functions can you use to round to the lowest integer value?",
+    choiceList : ["Math.random()", "Math.round()", "Math.ceil()", "Math.floor()"],
     correctAnswer : 3
 };
 
